3982aefddf9fdc931cd5b735bd49b7833708b38b
sorellan <s.salcedo.orellana@gmail.com>
2015-04-27 12:12:18 +0200

    Probando

--- github-api.js
+++ github-api.js
@@ -1,16 +1,27 @@
 var github;
 var repo;
 var results;
-var repoHTML = "User: <input type='text' name='user' value='sorellan' " +
+var repoData = "User: <input type='text' name='user' value='sorellan' " +
     "id='user' size='10' />" +
-    "Repo: <input type='text' name='repo' value='X-Nav-APIs-GitHub-Fichero' " +
+    "Repo: <input type='text' name='repo' value='X-Nav-OAuth-GitHub-Fichero' " +
     "id='repo' size='10' />" +
     "<button type='button'>Search repo data</button>";
 var repoFile = "File: <input type='text' name='file' value='filename' " +
     "id='file' size='10' />" +
-    "Content: <input type='text' name='content' value='content' " +
-    "id='content' size='10' />" +
+    "<button type='button' id='write'>Write File!</button>" +
+    "<button type='button' id='read'>Read File!</button><br/>" +
+    "Content: <textarea name='content' value='content' " +
+    "id='content' rows='4' cols='40' </textarea>" +
     "<button type='button'>Create file</button>";
+
+function login(network) {
+    var access = hello(network);
+    access.login({response_type: 'code'}).then( function(){
+        getToken(network);
+    }, function( e ){
+        alert('Signin error: ' + e.error.message);
+    });
+};
 
 function getToken(network) {
 	auth = hello(network).getAuthResponse();
@@ -19,7 +30,7 @@ function getToken(network) {
         token: token,
         auth: "oauth"
     });
-	$("#form_repo").html(repoHTML);
+	$("#form_repo").html(repoData);
 	$("#form_repo button").click(getRepo);
 };
 
@@ -37,11 +48,40 @@ function getRepo() {
                 "<ul><li>Full name: " + repo.full_name + "</li>" +
                 "<li>Description: " + repo.description + "</li>" +
                 "<li>Created at: " + repo.created_at + "</li>" +
-                "<li>Html url: <a href='" + repo.html_url + "'>" + repo.html_url + "</li></ul>");
-            $("#form_file").html(repoFile);
-            $("#form_file button").click(setFile);
+                "<li>Html url: <a href='" + repo.html_url + "'>" + repo.html_url + "</li>" + 
+                "<li><ul id='list_files'>Files:</ul></li></ul>");
+            listFiles();
+            //$("#form_file").html(repoFile);
+            //$("#form_file button").click(setFile);
         }
     });
+};
+
+function listFiles() {
+    repo.contents('master', '', function(error, contents) {
+        var repoList = $("#list_files");
+        if (error) {
+            repoList.html("<p>Error code: " + error.error + "</p>");
+        } else {
+            var files = [];
+            var len = contents.length;
+            for (var i = 0; i < len; i++) {
+                files.push(contents[i].name);
+            }
+            repoList.html("<li>" + 
+                files.join("</li><li>") +
+                "</li>");
+            $("#list_files li").click(selectFile);
+            $("#form_file").html(repoFile);
+            $("#write").click(setFile);
+            $("#read").click(readFile);
+        }
+    });
+};
+
+function selectFile() {
+    element = $(this);
+    $("#file").val(element.text());
 };
 
 function setFile() {
@@ -58,16 +98,7 @@ function setFile() {
 
 function readFile() {
     repo.read('master', $("#file").val(), function(err, data) {
-        results.append("<p>Contents:</p><p>" + data + "</p>");
-    });
-};
-
-function login(network) {
-    var access = hello(network);
-    access.login({response_type: 'code'}).then( function(){
-        getToken(network);
-    }, function( e ){
-        alert('Signin error: ' + e.error.message);
+        results.append("<p><b>Contents:</b></p><p>" + data + "</p>");
     });
 };
 
